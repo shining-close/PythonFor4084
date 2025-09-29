3f041b082441bd14af599074411e91fa2e2d883d
GCP <7726348465g@gmail.com>
2025-09-29 21:43:02 +0100

    first commit

--- Code/Example/Function/RandomFunction.py
+++ Code/Example/Function/RandomFunction.py
@@ -19,3 +19,12 @@ print(newrand, "\n")
 # Selects a random whole number between 0 and 9
 number4 = random.randint(0, 9)
 print(number4)
+
+
+# 使用整10的方式 pick a random number between 0 and 100 (inclusive) in steps of ten
+number5 = random.randrange(0 ,100 ,10)
+print(number5)
+
+# Select a colour randoly from the given colours
+colour = random.choice(["red", "blue", "green"])
+print(colour)
