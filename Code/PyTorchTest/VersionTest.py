d0cd4b48f64a92714f5da761ae0f1912d5b5240f
GCP <7726348465g@gmail.com>
2025-10-30 11:49:55 +0000

    1

--- Code/PyTorchTest/VersionTest.py
+++ Code/PyTorchTest/VersionTest.py
@@ -1,2 +1,14 @@
 import torch 
 print(torch.__version__)
+
+import pandas 
+print(pandas.__version__)
+
+import numpy
+print(numpy.__version__)
+
+import torch 
+print(torch.__version__)
+
+import torch 
+print(torch.__version__)
